9da4c558fde84d2dff4be40d22c54f716af096fe
Ryan Lynch <ReLynch91@gmail.com>
2020-05-13 14:33:01 -0700

    creating routes for api data fetches to be able to persist to database. Began to build schema for QuoteEndPointStock Model

---
+++ frontend/src/util/stock_util.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+`https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=${ticker}&apikey=demo`
+export const stockSaveToDB = (stockData) => {
+
+    return axios.post('/api/stocks', stockData);
+};
+
+// const baseUrl = 'http://localhost:3000/api/v1'
+// fetch(`${baseUrl}`).then(res => res)
+
+
+// need to update api key as well ^ 
--- routes/api/stocks.js
+++ routes/api/stocks.js
@@ -1,9 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const QuoteEndPointStock = require('../../models/stock_api_requests/QuoteEndPointStock');
 
 router.get("/test", (req, res) => res.json({
     msg: "This is the stocks route"
 }));
+
+router.post('/stocks', (req, res) => {
+    QuoteEndPointStock.findOne({ 
+        symbol: req.body.symbol
+    }).then(stock => {
+        if (!stock) {
+            const newStock = new QuoteEndPointStock({
+                symbol: req.body.symbol,
+                date: req.body.date,
+                details: req.body.details
+            })
+            return newStock;
+        } else {
+            stock.update
+        }
+    })
+});
 
 
 
