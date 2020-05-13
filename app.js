cdc446c57d3b31dda7eea728b88f59769f5d853b
Ryan Lynch <ReLynch91@gmail.com>
2020-05-12 17:32:59 -0700

    push to heroku

--- app.js
+++ app.js
@@ -11,15 +11,25 @@ const path = require('path');
 
 // app.get("/", (req, res) => res.send("Hello World"));
 // jason's comment
+
 mongoose
 .connect(db, {
     useNewUrlParser: true
-})
-.then(() => console.log("Connected to MongoDB successfully"))
+}).then(() => console.log("Connected to MongoDB successfully"))
 .catch(err => console.log(err));
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('frontend/build'));
+    app.get('/', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    })
+}
+
 app.use(passport.initialize());
+
 require('./config/passport')(passport);
+
+
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
