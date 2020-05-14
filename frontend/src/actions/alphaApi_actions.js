4cc3da469da241556eec35079acc3cf1198df686
Ryan Lynch <ReLynch91@gmail.com>
2020-05-14 16:51:40 -0700

    more formatting

--- frontend/src/actions/alphaApi_actions.js
+++ frontend/src/actions/alphaApi_actions.js
@@ -1,5 +1,4 @@
 import * as QuoteEndPointUtil from '../util/quote_end_point_util';
-import * as QuoteEndPointFormat from './_alphaApi_actions';
 import formatAPICall from './_alphaApi_actions';
 
 
@@ -12,20 +11,7 @@ import formatAPICall from './_alphaApi_actions';
 //     ))
 // );
 
-// {
-//     "Global Quote": {
-//         "01. symbol": "AAPL",
-//         "02. open": "304.5100",
-//         "03. high": "309.7900",
-//         "04. low": "301.5300",
-//         "05. price": "309.5400",
-//         "06. volume": "39653816",
-//         "07. latest trading day": "2020-05-14",
-//         "08. previous close": "307.6500",
-//         "09. change": "1.8900",
-//         "10. change percent": "0.6143%"
-//     }
-// }
+
 
 export const getQuoteEndPointAlpha = stockURL => dispatch => (
     QuoteEndPointUtil.QuoteEndPoint(stockURL).then((stockData) => ({
