4873d999ad6d604e712c0d868e4894276493e6ca
Ryan Lynch <ReLynch91@gmail.com>
2020-05-14 16:46:17 -0700

    working for the frontend

--- frontend/src/actions/alphaApi_actions.js
+++ frontend/src/actions/alphaApi_actions.js
@@ -1,4 +1,6 @@
 import * as QuoteEndPointUtil from '../util/quote_end_point_util';
+import * as QuoteEndPointFormat from './_alphaApi_actions';
+import formatAPICall from './_alphaApi_actions';
 
 
 // Upon signup, dispatch the approporiate action depending on which type of response we receieve from the backend
@@ -10,6 +12,28 @@ import * as QuoteEndPointUtil from '../util/quote_end_point_util';
 //     ))
 // );
 
+// {
+//     "Global Quote": {
+//         "01. symbol": "AAPL",
+//         "02. open": "304.5100",
+//         "03. high": "309.7900",
+//         "04. low": "301.5300",
+//         "05. price": "309.5400",
+//         "06. volume": "39653816",
+//         "07. latest trading day": "2020-05-14",
+//         "08. previous close": "307.6500",
+//         "09. change": "1.8900",
+//         "10. change percent": "0.6143%"
+//     }
+// }
+
 export const getQuoteEndPointAlpha = stockURL => dispatch => (
-    QuoteEndPointUtil.QuoteEndPoint(stockURL)
+    QuoteEndPointUtil.QuoteEndPoint(stockURL).then((stockData) => ({
+        let formatted = formatAPICall(stockData);
+
+        QuoteEndPointUtil.quoteEndPointDB(stockData).then(stockData => (
+                )
+    }
+        
+    ));
 )
--- frontend/src/components/stocks/stock_search/stock_search_container.js
+++ frontend/src/components/stocks/stock_search/stock_search_container.js
@@ -1,12 +1,15 @@
-// import { connect } from "react-redux";
-// import StockSearch from "./stock_search";
+import { connect } from "react-redux";
+import StockSearch from "./stock_search";
+import { getQuoteEndPointAlpha } from '../../../actions/alphaApi_actions'
 
-// const mapStateToProps = (state) => ({
+const mapStateToProps = (state) => ({
   
-// });
+});
 
-// const mapDispatchToProps = (dispatch) => ({
-//     fetchThisStock: "Insert your axios api call here"
-// })
+const mapDispatchToProps = (dispatch) => ({
+    // fetchThisStock: "Insert your axios api call here"
+    getQuoteEndPointAlpha: (stockURL) => dispatch(getQuoteEndPointAlpha(stockURL))
+    // logout: () => dispatch(logout()),
+})
 
-// export default connect(null, mapDispatchToProps)(StockSearch);
+export default connect(null, mapDispatchToProps)(StockSearch);
