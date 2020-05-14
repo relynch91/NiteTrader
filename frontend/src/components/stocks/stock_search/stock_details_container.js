a7ebd7474d5d1dc18685e30e05e3701579f70953
Ben Hafner <brhafner@gmail.com>
2020-05-14 14:27:10 -0700

    Rough structure of StockIndex and Stock Search

---
+++ frontend/src/components/stocks/stock_search/stock_details_container.js
@@ -0,0 +1,12 @@
+// import { connect } from "react-redux";
+// import StockDetails from "./stock_details";
+
+// const mapStateToProps = (state) => ({
+//     stock
+// });
+
+// const mapDispatchToProps = (dispatch) => ({
+//   fetchThisStock: "Insert your axios api call here",
+// });
+
+// export default connect(null, mapDispatchToProps)(StockSearch);
---
+++ frontend/src/components/stocks/stock_search/stock_search.js
@@ -0,0 +1,35 @@
+// import React from 'react';
+// import StockDetails from './stock_details'
+
+// export default class StockSearch extends React.Component {
+//     constructor(props){
+//         super(props)
+//         this.state = {
+//             stockDetails = {}
+//         }
+//     }
+
+
+//  getStockDetails(){
+//         // dispatch api call (comes from mdtp from axios)
+//             // .then (returnedStockInfo => this.setState(returnedStockInfoDetails: stock))
+//     }
+
+//     render(){
+//         let stockDetails = this.state;
+//         let theDetails = (!!stockDetails ? <StockDetails stockDetails={stockDetails} /> : "")
+        
+
+//             return(
+//                 <div>
+//                     <form >
+//                         <label>Enter a Stock Ticker
+//                             <input type="text" value=""/>
+//                         </label>
+//                         <button onClick={this.props.getStockDetails()}>Lookup Stock</button>
+//                     </form>
+//                     {theDetails}
+//                 </div>
+//             )
+//     }
+// }
