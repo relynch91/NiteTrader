6a7dfee1dd94eed319f4efdb02b20297ff26558f
Ryan Lynch <ReLynch91@gmail.com>
2020-05-30 16:39:06 -0700

    add more files

--- routes/api/stock_api/intradayapi.js
+++ routes/api/stock_api/intradayapi.js
@@ -6,4 +6,23 @@ router.get("/test", (req, res) => res.json({
     msg: "This is the IntraDayAPI AlphAdvantage Stock route"
 }));
 
+router.patch('/update', (req, res) => {
+    const query = {
+        symbol: req.body.symbol
+    };
+
+    const update = {
+        symbol: req.body.symbol,
+        timeSeries: req.body.timeSeries
+    };
+
+    const updatedStock = IntraDayAPI.replaceOne(query, update);
+
+    updatedStock.then(updatedStock => res.json(updatedStock));
+
+    // Need to figure out how to re-render the updateStock. 
+    // need to dispatch a get request for the updated stock. 
+    // current ouput is the db response to the successful update.  Need to re-render 
+})
+
 module.exports = router;
