a6f76d0f1e97c1fceae14aedeab88519c6f09bf5
Ben Hafner <brhafner@gmail.com>
2020-06-04 14:18:02 -0700

    Add partial functionality for Stock Index, render still broken

--- frontend/src/components/stocks/stock_index/stock_index.js
+++ frontend/src/components/stocks/stock_index/stock_index.js
@@ -1,36 +1,49 @@
 import React from 'react';
 import './stock_index.css';
+import * as TransUtil from '../../../util/transaction_api_util';
 
 export default class StockIndex extends React.Component {
     constructor(props){
       super(props)
-      this.state = {};
     }
+
     componentDidMount(){
-      // debugger
-      let trades = this.props.fetchTrades(this.props.userId)
-          // .then(res => this.setState(res))
+      let {fetchTrades, userId, myStocks} = this.props
+      fetchTrades(userId)
+        // .then(() => this.setState( myStocks ))
         // .then(() => console.log(this.state))
     }
 
+    // componentDidUpdate(){
+    //   debugger
+    //   let { myStocks, activeShares } = this.props;
+    //   this.setState({mystocks: activeShares(myStocks)})
+    // }
+    
     render(){
-            let { myStocks } = this.props;
-            return (
+            let myShares = !!this.props.myStocks ? TransUtil.activeShares(this.props.myStocks) : null;
+            let theStuff = (this.props.myStocks === undefined) ? null : (
               <div className="stock-index-main">
                 <p>Here is Your Current Stock Portfolio</p>
-                {myStocks.map((ticker) => (
+                {Object.keys(myShares).map((ticker) => (
                   <div>
                     <div className="stock-box">
                       <span>{ticker}</span>
                       <span>
                         <p className="current-price">Current Price: $214</p>
                         <p className="purchase-price">Purchase Price: $110</p>
-                      {/* onClick= dispatch past month of data to populate graph */}
+                        {/* onClick= dispatch past month of data to populate graph */}
                       </span>
                     </div>
                   </div>
                 ))}
               </div>
+            )
+            // debugger
+            return (
+              <div>
+                {theStuff}
+              </div>
             );
-    }
+          }
 }
--- frontend/src/components/stocks/stock_index/stock_index_container.js
+++ frontend/src/components/stocks/stock_index/stock_index_container.js
@@ -1,15 +1,18 @@
 import { connect } from "react-redux";
 import StockIndex from './stock_index'
 import { fetchTrades } from '../../../actions/transaction_actions'
+import { activeShares } from '../../../util/transaction_api_util'
 
 const mapStateToProps = (state) => ({
-  myStocks: ["AAPL", "IBM", "TSLA", "NKE"],
+  myStocks: state.transactions,
+  // myStocks: ["AAPL", "IBM", "TSLA", "NKE"],
   // myStocks: state.transactions
   userId: state.session.user.id 
 });
 
 const mapDispatchToProps = dispatch => ({
-  fetchTrades: (userId) => dispatch(fetchTrades(userId)) 
+  fetchTrades: (userId) => dispatch(fetchTrades(userId)),
+  activeShares: (transactions) => dispatch(activeShares(transactions))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(StockIndex);
