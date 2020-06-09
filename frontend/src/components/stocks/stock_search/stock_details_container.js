ea5a7051b4d3d070c640c02dfaa0c26c5ca735ac
Ben Hafner <brhafner@gmail.com>
2020-06-09 15:39:21 -0700

    add conditional rendering for stock details with mstp keyed into globalEndPoint

--- frontend/src/components/stocks/stock_search/stock_details_container.js
+++ frontend/src/components/stocks/stock_search/stock_details_container.js
@@ -4,7 +4,7 @@ import StockDetails from "./stock_details";
 import { tradeStock } from '../../../actions/transaction_actions'
 
 const mapStateToProps = (state) => ({
-    stockDetails: state.stocks.stock1,
+    stockDetails: state.stocks,
     userId: state.session.user.id
 });
 
