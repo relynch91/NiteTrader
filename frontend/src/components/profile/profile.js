4162bfcd3177f6e7b419fd7d11d39d091b086748
Ryan Lynch <ReLynch91@gmail.com>
2020-06-12 11:33:49 -0700

    work on formatting some more

--- frontend/src/components/profile/profile.js
+++ frontend/src/components/profile/profile.js
@@ -1,30 +1,27 @@
 import React from 'react';
 
 class Profile extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            stocks: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      stocks: []
     }
-    
-    componentWillMount() {
-        console.log(this.props.currentUser.id)
-    }
+  }
 
-    render() {
-        if (this.state.stocks.length === 0) {
-          return (<div>This user has no Stocks</div>)
-        } else {
-          return (
-            <div>
-              <h2>All of This User's Stocks</h2>
-                <ul>
-                    {this.state.stocks.map(stock =>(<li>{stock}</li>))}
-                </ul>
-            </div>
-          );}}
+  render() {
+    if (this.state.stocks.length === 0) {
+      return (<div>This user has no Stocks</div>)
+    } else {
+      return (
+        <div>
+          <h2>All of This User's Stocks</h2>
+            <ul>
+                {this.state.stocks.map(stock =>(<li>{stock}</li>))}
+            </ul>
+        </div>
+      );
+    }
+  }
 }
 
 export default Profile;
