7154e6568e8996e2a45b314abb8531a65548f234
Ben Hafner <brhafner@gmail.com>
2020-06-26 14:47:59 -0700

    Code cleanup and set upstream

--- frontend/src/components/stocks/stock_search/stock_name.js
+++ frontend/src/components/stocks/stock_search/stock_name.js
@@ -38,27 +38,3 @@ export default class StockName extends React.Component {
     }
 }
 
-
-
-// {
-//     Object.keys(myPortfolio).map((ticker, idx) => (
-//         <div className="stock-box-owned" key={idx * 392}>
-//             <div className='stock-box-owned-ticker'>Ticker: {ticker}</div>
-//             <ul className='stock-box-owned-details'>
-//                 < li className='stock-boxowned-current-price'>
-//                     Price Per Share:
-//                 ${(parseFloat(myPortfolio[ticker].pricePerShare).toFixed(2))}
-//                 </li>
-//                 < li className="stock-box-owned-purchase-price">
-//                     Shares Owned: {myPortfolio[ticker].ownedShares.toFixed(2)}
-//                 </li>
-//                 {(myPortfolio[ticker]['priceDiff'] > 0) ?
-//                     <li className='stock-box-owned-price-change-positive'>
-//                         Gain Per Share: ${myPortfolio[ticker]['priceDiff'].toFixed(2)}
-//                     </li> :
-//                     <li className='stock-box-owned-price-change-negative'>
-//                         Loss Loss Per Share: ${myPortfolio[ticker]['priceDiff']}
-//                     </li>
-//                 }
-//             </ul>
-//         </div>
