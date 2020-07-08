ce048efa0c43cd991330318b6693010fdff42866
Ryan Lynch <ReLynch91@gmail.com>
2020-07-07 17:53:31 -0700

    more stuff

--- frontend/src/components/nav/navbar.js
+++ frontend/src/components/nav/navbar.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, NavLink } from 'react-router-dom'
 import './navbar.css'
-import { session } from 'passport';
 
 class NavBar extends React.Component {
   constructor(props) {
