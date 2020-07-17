32e6c71ec7be50f9dcb9ba3abadb125341f1d597
Ryan Lynch <ReLynch91@gmail.com>
2020-07-17 14:31:51 -0700

    Profile Table

--- frontend/src/components/profile/profile_data.js
+++ frontend/src/components/profile/profile_data.js
@@ -5,14 +5,23 @@ class ProfileData extends React.Component {
   constructor(props) {
     super(props)
     this.state = {};
+    this.buyOrSell = this.buyOrSell.bind(this);
   }
 
   componentDidMount() {
     this.props.buildProfile(this.props.myPortfolio);
   }
 
+  buyOrSell(value) {
+    if (value) {
+      return 'Buy'
+    };
+    return 'Sell';
+  }
+
   render() {
     let transactions = this.props.trades;
+    debugger
     // <div className='stock-search-landing-results'>
     //    <h1>Click on a company below to view stock data</h1>
     //    <ul className='stock-search-results-container'>
@@ -36,7 +45,20 @@ class ProfileData extends React.Component {
         </div>
         <div className='profile-transactions'> 
           <h1>Your Transactions: </h1>
-
+            <ul className='profile-transactions-container'>
+              {Object.keys(transactions).map((compObj, idx) => {
+                return (
+                  <div className='profile-transaction-info'>
+                    <li className = 'profile-transaction-list' key={idx*29}>
+                      <h4>Ticker: {transactions[compObj]['ticker']} </h4>
+                      <h4>Date: {transactions[compObj]['date'].split('T')[0]} </h4>
+                      <h4>Type: {this.buyOrSell(transactions[compObj]['buy'])} </h4>
+                      <h4>Price: {transactions[compObj]['price']} </h4>
+                      <h4>Shares: {transactions[compObj]['shares']} </h4>
+                    </li>
+                  </div>
+              )})}
+            </ul>
         </div>
       </div>
     )  
