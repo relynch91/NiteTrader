b131f54490c033bc3542a22596ad91cd04be548c
Ryan Lynch <ReLynch91@gmail.com>
2020-08-25 17:11:54 -0700

    add files

--- dynoWorker.js
+++ dynoWorker.js
@@ -62,65 +62,65 @@ candle();
 
 // --------------------- above updates DB w/ all tickers ---------------------
 
-async function updatePortfolio(updatedTestData) { // test data ticker(key) price(value);
-    let theKeys = (updatedTestData); //theKeys are all the tickers
-    let users = await axios.get('https://nitetrader.herokuapp.com/api/users/allusernames')
+// async function updatePortfolio(updatedTestData) { // test data ticker(key) price(value);
+//     let theKeys = (updatedTestData); //theKeys are all the tickers
+//     let users = await axios.get('https://nitetrader.herokuapp.com/api/users/allusernames')
 
-    let userIds = [];
-    users.data.forEach(obj => {
-        userIds.push(obj._id);
-    })
+//     let userIds = [];
+//     users.data.forEach(obj => {
+//         userIds.push(obj._id);
+//     })
 
-    for (let i = 0; i < userIds.length; i ++) {
-        let response = await axios.get(
-            `https://nitetrader.herokuapp.com/api/transactions/${userIds[i]}`)
-        let tickerSharesObj = sortResponse(response.data);
-        let userValue = calculateValue(tickerSharesObj, theKeys);
-        let userID = userIds[i]
-        let userCash = await axios.get( 
-            `https://nitetrader.herokuapp.com/api/stat/${userID}`)
-        .catch(error => console.log(error))
-        if (userCash.data.length === 0) {
-            userCash.data[0] = {'value': 50000};
-        }
-        let profileInfo = {
-            userID: userIds[i],
-            value: userValue,
-            cash: parseFloat(userCash.data[0]['value'])
-        }
+//     for (let i = 0; i < userIds.length; i ++) {
+//         let response = await axios.get(
+//             `https://nitetrader.herokuapp.com/api/transactions/${userIds[i]}`)
+//         let tickerSharesObj = sortResponse(response.data);
+//         let userValue = calculateValue(tickerSharesObj, theKeys);
+//         let userID = userIds[i]
+//         let userCash = await axios.get( 
+//             `https://nitetrader.herokuapp.com/api/stat/${userID}`)
+//         .catch(error => console.log(error))
+//         if (userCash.data.length === 0) {
+//             userCash.data[0] = {'value': 50000};
+//         }
+//         let profileInfo = {
+//             userID: userIds[i],
+//             value: userValue,
+//             cash: parseFloat(userCash.data[0]['value'])
+//         }
         
-        await axios.post('https://nitetrader.herokuapp.com/api/profile/new', 
-            profileInfo).catch(err => console.log(err))
-    }
-    console.log('The candle has been lit');
-    return true
-}
+//         await axios.post('https://nitetrader.herokuapp.com/api/profile/update', 
+//             profileInfo).catch(err => console.log(err))
+//     }
+//     console.log('The candle has been lit');
+//     return true
+// }
 
-function sortResponse(transactions) { //transactions is an array of objects that are transactions
-    let tickers = {};
-    for (let i = 0; i < transactions.length; i ++) {
-        if (transactions[i]['buy']) {
-            let ticker = transactions[i]['ticker'];
-            tickers[ticker] = 0;
-            tickers[ticker] += transactions[i]['shares'];
-        }
-    }
-    return tickers;
-}
+// function sortResponse(transactions) { //transactions is an array of objects that are transactions. Nice. 
+//     let tickers = {};
+//     for (let i = 0; i < transactions.length; i ++) {
+//         if (transactions[i]['buy']) {
+//             let ticker = transactions[i]['ticker'];
+//             tickers[ticker] = 0;
+//             tickers[ticker] += transactions[i]['shares'];
+//         }
+//     }
+//     return tickers;
+// }
 
-function calculateValue (tickerSharesObj, theKeys) {
-    let totalValue = 0;
-    let ownedStocks = Object.keys(tickerSharesObj);
-    if (ownedStocks.length === 0) {
-        return 0;
-    }
-    for (let i = 0; i < ownedStocks.length; i ++) {
-        let ticker = ownedStocks[i];
-        let quantity = parseFloat(tickerSharesObj[ticker]);
-        let value = parseFloat(theKeys[ticker]);
-        totalValue += (quantity * value);
-    }
-    return totalValue;
-}
+// function calculateValue (tickerSharesObj, theKeys) {
+//     let totalValue = 0;
+//     let ownedStocks = Object.keys(tickerSharesObj);
+//     if (ownedStocks.length === 0) {
+//         return 0;
+//     }
+//     for (let i = 0; i < ownedStocks.length; i ++) {
+//         let ticker = ownedStocks[i];
+//         let quantity = parseFloat(tickerSharesObj[ticker]);
+//         let value = parseFloat(theKeys[ticker]);
+//         totalValue += (quantity * value);
+//     }
+//     return totalValue;
+// }
 
 // updatePortfolio(updatedTestData)
