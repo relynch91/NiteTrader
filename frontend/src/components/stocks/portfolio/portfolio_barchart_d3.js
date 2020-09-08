4cc7b80201fc8d96dce654e683db102ee4fc5e64
Ryan Lynch <ReLynch91@gmail.com>
2020-09-08 14:56:07 -0700

    add files

--- frontend/src/components/stocks/portfolio/portfolio_barchart_d3.js
+++ frontend/src/components/stocks/portfolio/portfolio_barchart_d3.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import * as d3 from 'd3';
+import { scaleLinear } from 'd3-scale';
+import { select } from 'd3-selection'
 import { formatPortfolioData, createMinMax } from '../../../util/portfolio_api_util'
 
 
@@ -11,10 +13,6 @@ class PortfolioD3BarChart extends React.Component {
 
     componentDidMount() {
         this.createBarChart();
-        let data = formatPortfolioData(this.props.ownedStocks);
-        let minMax = createMinMax(data);
-        console.log(minMax);
-        
     }
 
     componentDidUpdate() {
@@ -22,7 +20,35 @@ class PortfolioD3BarChart extends React.Component {
     }
 
     createBarChart() {
+        let data = formatPortfolioData(this.props.ownedStocks);
+        let minMax = createMinMax(data);
+        console.log(minMax);
 
+        const node = this.node
+        const yScale = scaleLinear()
+            .domain([minMax[0], minMax[1]])
+            .range([minMax[0], minMax[1]])
+
+        select(node)
+            .selectAll('rect')
+            .data(this.props.data)
+            .enter()
+            .append('rect')
+
+        select(node)
+            .selectAll('rect')
+            .data(this.props.data)
+            .exit()
+            .remove()
+
+        select(node)
+            .selectAll('rect')
+            .data(this.props.data)
+            .style('fill', '#fe9922')
+            .attr('x', (d, i) => i * 25)
+            .attr('y', d => minMax[1] - yScale(d))
+            .attr('height', d => yScale(d))
+            .attr('width', 25)
     }
     
     render() {
