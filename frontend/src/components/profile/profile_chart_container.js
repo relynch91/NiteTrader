322812703fc6f3311afb41ebe6f47e554ce68b9a
Ryan Lynch <ReLynch91@gmail.com>
2020-09-11 19:05:59 -0700

    add files

--- frontend/src/components/profile/profile_chart_container.js
+++ frontend/src/components/profile/profile_chart_container.js
@@ -1,21 +1,14 @@
 import { connect } from 'react-redux';
 import ProfileChart from './profile_chart';
-import { fetchTrades } from '../../actions/transaction_actions';
-import { buildPortfolio } from '../../actions/portfolio_actions'
-
 
 const mapStateToProps = (state) => {
   return {
-    myStocks: state.transactions,
-    myPortfolio: state.portfolio,
     userId: state.session.user.id,
-    trades: state.transactions
   };
 };
 
 const mapDispatchToProps = dispatch => ({
-  fetchTrades: (userId) => dispatch(fetchTrades(userId)),
-  buildPortfolio: (transactions) => dispatch(buildPortfolio(transactions))
+  
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(ProfileChart);
